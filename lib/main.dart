9e3223f091b0ce01e01762d5f597fb43f06a53dc
barbara.manhaes <barbara.manhaes@hotmail.com>
2026-04-13 13:10:06 +0000

    Semana 7

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+    void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -109,14 +115,11 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            TextButton(onPressed: _incrementCounter, child: Text('Increment')),
+           TextButton(onPressed: _decrementCounter, child: Text('Decrement')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
